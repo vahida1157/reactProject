aa33151680b59add37f7c909e672231cf1450eaf
vahida1157 <vahidghasemi119@gmail.com>
2021-12-01 02:27:54 +0330

    play with code

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Button extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>salam</h1>
+                <input value='salam' type='button' className='btn'/>
+            </div>
+        );
+    }
+}
+
+export default Button
